45f6b956ec71d463d8478150caa2dcf6ab30410b
JaeD015 <146486823+JaeD015@users.noreply.github.com>
2023-10-16 16:24:22 -0500

    Add files via upload

---
+++ rd3.py
@@ -0,0 +1,22 @@
+import random
+#https://www.w3schools.com/python/python_casting.asp
+dieRolls= input('How many time would you like to roll?')
+
+dieRollsInteger=int(dieRolls)
+
+diceSides=input ('How many sides would you like?')
+
+diceSidesInteger=int(diceSides)
+
+for rolls in range(dieRollsInteger):
+    randomDice = random.randrange(diceSidesInteger)+1
+    print(randomDice)
+
+    if diceSidesInteger==20: 
+        if randomDice==20:
+            print('critical hit')
+        if randomDice==1:
+            print('critical miss')
+    
+print('look at you go!')
+
